deab5de79ef1fc8dea285c00a92436546ae8631a
obada404 <oday.qr.2001@gmail.com>
2022-04-02 00:11:14 +0300

    aa

--- backs2/app.js
+++ backs2/app.js
@@ -16,11 +16,11 @@ app.get("/purchase", async (req, res) => {
   let q = url.parse(req.url, true);
   let qdata = q.query;
   try {
-    const req1= await axios.get('http://'+req.socket.localAddress+':8000/info?Id='+qdata.Id);
+    const req1= await axios.get('http://192.168.1.104:8000/info?Id='+qdata.Id);
     console.log(req1.data ,"req1");
     let req2 ;
     if(req1.data.stock >0){
-      req2 =await axios.get('http://'+req.socket.localAddress+':8000'+req.url);
+      req2 =await axios.get('http://192.168.1.104:8000'+req.url);
       console.log(req2.data ,"req2");
   
     }
